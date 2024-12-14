d28dd5de24f88a307828f3512c8173e2977cd7d2
Edeman George <71378676+eds-101@users.noreply.github.com>
2024-12-14 08:46:17 +0000

    build fix - env environment updates

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,8 @@
 export const appScope = "user-read-private user-read-email user-top-read";
 export const cookieMaxAge = 43140;
-export const redirectUri = "http://localhost:8888/callback";
 export const sessionCookie = "session";
+
+const domain = import.meta.env.DOMAIN || window.location.origin || "http://localhost:8888";
+export const redirectUri = `${domain}/callback`;
+
+
